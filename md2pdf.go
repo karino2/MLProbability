6ae310a31549111457e59e9081979fd086d5ee72
karino2 <hogeika2@gmailcom>
2018-05-25 18:19:45 +0900

    Implement md2pdf, not yet tested.

---
+++ md2pdf.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+
+	file, err := os.Open("contents.txt")
+	if err != nil {
+		panic("can't open content.txt")
+	}
+
+	defer file.Close()
+
+	pandocargs := make([]string, 0, 10)
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		pandocargs = append(pandocargs, scanner.Text())
+	}
+
+	fixed := []string{"-o", "MLProbability.pdf",
+		"-V", "documentclass=ltjarticle",
+		"-V", "classoption=a4j",
+		"-V", "geometry:margin=1in",
+		"--pdf-engine=lualatex"}
+
+	pandocargs = append(pandocargs, fixed...)
+	// fmt.Println(strings.Join(pandocargs, " "))
+
+	// -o p_space.pdf -V documentclass=ltjarticle -V classoption=a4j -V geometry:margin=1in --pdf-engine=lualatex
+	cmd := exec.Command("pandoc", pandocargs...)
+
+	if err = cmd.Run(); err != nil {
+		panic("cant execute pandoc " + strings.Join(pandocargs, " "))
+	}
+
+}
